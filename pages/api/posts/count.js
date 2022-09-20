1e8d512cada7336c518a94ea82deb1b48848edc2
Dylan Pincus <dpincus95@gmail.com>
2022-09-19 21:09:02 -0400

    Send count response

--- pages/api/posts/count.js
+++ pages/api/posts/count.js
@@ -7,5 +7,7 @@ module.exports = withApiAuthRequired(async (req, res) => {
     res.status(200).json(count).send();
   } catch (error) {
     res.status(500);
-  }
+  } finally(() => {
+    res.send()
+  })
 });
